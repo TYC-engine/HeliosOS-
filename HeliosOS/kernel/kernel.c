b0f8e9243a29c1410eafd867d5f77e284b605911
TYC-engine <dema29992@gmail.com>
2026-06-14 00:58:35 +0600

    Update kernel.c

--- HeliosOS/kernel/kernel.c
+++ HeliosOS/kernel/kernel.c
@@ -1,4 +1,6 @@
 #include "screen.h"
+#include "keyboard.h"
+#include "pit.h"
 #include "idt.h"
 
 extern void pic_remap();
@@ -7,18 +9,24 @@ void kernel_main()
 {
     clear_screen();
 
-    print("HeliosOS\n");
+    print("HeliosOS Kernel Booted\n");
 
     pic_remap();
-
     idt_init();
+    pit_init();
 
     __asm__ volatile("sti");
 
-    print("Interrupts enabled\n");
+    print("System ready\n");
+    print("helios> ");
 
     while(1)
     {
+        char c = keyboard_read();
+
+        if(c)
+            putchar(c);
+
         __asm__ volatile("hlt");
     }
 }
