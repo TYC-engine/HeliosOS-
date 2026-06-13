4823bb0b7088e9469077eaaca81de9ec36c9b877
TYC-engine <dema29992@gmail.com>
2026-06-14 00:47:48 +0600

    Update keyboard.c

--- HeliosOS/kernel/keyboard.c
+++ HeliosOS/kernel/keyboard.c
@@ -1,13 +1,31 @@
 #include "ports.h"
-#include "screen.h"
+
+static char last_key = 0;
+
+static const char keymap[128] =
+{
+    0,27,'1','2','3','4','5','6',
+    '7','8','9','0','-','=',
+    '\b','\t',
+    'q','w','e','r','t','y','u','i',
+    'o','p','[',']','\n',0,
+    'a','s','d','f','g','h','j','k',
+    'l',';','\'','`',0,'\\',
+    'z','x','c','v','b','n','m',',',
+    '.','/',0,'*',0,' '
+};
 
 void keyboard_irq()
 {
-    unsigned char scancode;
+    unsigned char scancode = inb(0x60);
 
-    scancode = inb(0x60);
+    if(scancode < 128)
+        last_key = keymap[scancode];
+}
 
-    print("K");
-
-    outb(0x20,0x20);
+char keyboard_get_last()
+{
+    char c = last_key;
+    last_key = 0;
+    return c;
 }
