7713415457981dc4046db0352ac696bf4b46c2bd
TYC-engine <dema29992@gmail.com>
2026-06-14 01:06:12 +0600

    Update idt.c

--- HeliosOS/kernel/idt.c
+++ HeliosOS/kernel/idt.c
@@ -1,34 +1,32 @@
 #include "idt.h"
 
 extern void isr0();
+extern void isr1();
 extern void irq0();
 extern void irq1();
 
 static idt_entry_t idt[256];
 static idt_ptr_t idtp;
 
-static void idt_set_gate(
-    int num,
-    uint32_t base,
-    uint16_t sel,
-    uint8_t flags)
+static void set_gate(int n, uint32_t handler)
 {
-    idt[num].offset_low = base & 0xFFFF;
-    idt[num].selector = sel;
-    idt[num].zero = 0;
-    idt[num].flags = flags;
-    idt[num].offset_high = base >> 16;
+    idt[n].low = handler & 0xFFFF;
+    idt[n].sel = 0x08;
+    idt[n].zero = 0;
+    idt[n].flags = 0x8E;
+    idt[n].high = handler >> 16;
 }
 
 void idt_init()
 {
-    idtp.limit = sizeof(idt)-1;
+    idtp.limit = sizeof(idt) - 1;
     idtp.base = (uint32_t)&idt;
 
-    idt_set_gate(0,(uint32_t)isr0,0x08,0x8E);
+    set_gate(0, (uint32_t)isr0);
+    set_gate(1, (uint32_t)isr1);
 
-    idt_set_gate(32,(uint32_t)irq0,0x08,0x8E);
-    idt_set_gate(33,(uint32_t)irq1,0x08,0x8E);
+    set_gate(32, (uint32_t)irq0);
+    set_gate(33, (uint32_t)irq1);
 
     __asm__ volatile("lidt %0" : : "m"(idtp));
 }
