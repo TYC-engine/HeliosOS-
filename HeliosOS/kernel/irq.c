0308b79b9d121b9822a2abb0779427a46adc0acc
TYC-engine <dema29992@gmail.com>
2026-06-14 00:58:09 +0600

    Update irq.c

--- HeliosOS/kernel/irq.c
+++ HeliosOS/kernel/irq.c
@@ -1,19 +1,14 @@
-#include "ports.h"
+#include "screen.h"
+#include "keyboard.h"
+#include "pit.h"
 
-void pic_remap()
+void irq_handler(int irq)
 {
-    outb(0x20,0x11);
-    outb(0xA0,0x11);
+    if(irq == 0)
+        pit_irq();
 
-    outb(0x21,0x20);
-    outb(0xA1,0x28);
+    if(irq == 1)
+        keyboard_irq();
 
-    outb(0x21,0x04);
-    outb(0xA1,0x02);
-
-    outb(0x21,0x01);
-    outb(0xA1,0x01);
-
-    outb(0x21,0x00);
-    outb(0xA1,0x00);
+    outb(0x20, 0x20);
 }
