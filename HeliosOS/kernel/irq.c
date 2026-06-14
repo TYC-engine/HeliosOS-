e0212483511d1daa596f94e2b5834907577b0353
TYC-engine <dema29992@gmail.com>
2026-06-14 12:30:18 +0600

    Update irq.c

--- HeliosOS/kernel/irq.c
+++ HeliosOS/kernel/irq.c
@@ -1,14 +1,33 @@
-#include "screen.h"
-#include "keyboard.h"
-#include "pit.h"
+#include "ports.h"
 
-void irq_handler(int irq)
+#define PIC1 0x20
+#define PIC2 0xA0
+#define PIC1_CMD PIC1
+#define PIC1_DATA (PIC1+1)
+#define PIC2_CMD PIC2
+#define PIC2_DATA (PIC2+1)
+
+#define ICW1_INIT 0x10
+#define ICW1_ICW4 0x01
+#define ICW4_8086 0x01
+
+void pic_remap()
 {
-    if(irq == 0)
-        pit_irq();
+    uint8_t a1 = inb(PIC1_DATA);
+    uint8_t a2 = inb(PIC2_DATA);
 
-    if(irq == 1)
-        keyboard_irq();
+    outb(PIC1_CMD, ICW1_INIT | ICW1_ICW4);
+    outb(PIC2_CMD, ICW1_INIT | ICW1_ICW4);
 
-    outb(0x20, 0x20);
+    outb(PIC1_DATA, 0x20); // IRQ0 -> INT 32
+    outb(PIC2_DATA, 0x28); // IRQ8 -> INT 40
+
+    outb(PIC1_DATA, 4);
+    outb(PIC2_DATA, 2);
+
+    outb(PIC1_DATA, ICW4_8086);
+    outb(PIC2_DATA, ICW4_8086);
+
+    outb(PIC1_DATA, a1);
+    outb(PIC2_DATA, a2);
 }
