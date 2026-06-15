4e2871b0ae2d4d489c3dfe80f343b20d03aa06c2
TYC-engine <dema29992@gmail.com>
2026-06-15 14:10:16 +0600

    Update heap.h

--- HeliosOS/kernel/memory/heap.h
+++ HeliosOS/kernel/memory/heap.h
@@ -3,12 +3,26 @@
 
 #include <stdint.h>
 
-void heap_init(uint32_t start, uint32_t size);
+typedef struct heap_block
+{
+    uint32_t size;
+    uint8_t free;
+
+    struct heap_block* next;
+    struct heap_block* prev;
+
+} heap_block_t;
+
+void heap_init(uint32_t heap_start, uint32_t heap_size);
 
 void* kmalloc(uint32_t size);
 void* kcalloc(uint32_t count, uint32_t size);
 void* krealloc(void* ptr, uint32_t size);
 
 void kfree(void* ptr);
+
+uint32_t heap_total_memory();
+uint32_t heap_used_memory();
+uint32_t heap_free_memory();
 
 #endif
