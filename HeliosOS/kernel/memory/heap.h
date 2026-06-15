82f616dcacec3a904f6c8a1dfc607da9f7a87237
TYC-engine <dema29992@gmail.com>
2026-06-15 14:07:21 +0600

    Add kcalloc and krealloc function declarations

--- HeliosOS/kernel/memory/heap.h
+++ HeliosOS/kernel/memory/heap.h
@@ -6,6 +6,9 @@
 void heap_init(uint32_t start, uint32_t size);
 
 void* kmalloc(uint32_t size);
+void* kcalloc(uint32_t count, uint32_t size);
+void* krealloc(void* ptr, uint32_t size);
+
 void kfree(void* ptr);
 
 #endif
