ea4f999eb8616a5f9f518030d0f75c0d67939691
TYC-engine <dema29992@gmail.com>
2026-06-15 14:21:31 +0600

    Update heap.c

--- HeliosOS/kernel/memory/heap.c
+++ HeliosOS/kernel/memory/heap.c
@@ -1,68 +1,73 @@
 #include "heap.h"
 
-typedef struct block
+static heap_block_t* head = 0;
+
+static uint32_t heap_start_addr = 0;
+static uint32_t heap_total_size = 0;
+
+static uint32_t used_memory = 0;
+
+static void memset8(uint8_t* p, uint8_t v, uint32_t s)
 {
-    uint32_t size;
-    uint8_t free;
-
-    struct block* next;
-} block_t;
-
-static block_t* heap_head = 0;
-
-static void memory_copy(uint8_t* dst, uint8_t* src, uint32_t size)
-{
-    while(size--)
-        *dst++ = *src++;
+    for(uint32_t i = 0; i < s; i++)
+        p[i] = v;
 }
 
-static void memory_set(uint8_t* ptr, uint8_t value, uint32_t size)
+static void memcpy8(uint8_t* d, uint8_t* s, uint32_t size)
 {
-    while(size--)
-        *ptr++ = value;
+    for(uint32_t i = 0; i < size; i++)
+        d[i] = s[i];
 }
 
 void heap_init(uint32_t start, uint32_t size)
 {
-    heap_head = (block_t*)start;
+    heap_start_addr = start;
+    heap_total_size = size;
 
-    heap_head->size = size - sizeof(block_t);
-    heap_head->free = 1;
-    heap_head->next = 0;
+    head = (heap_block_t*)start;
+
+    head->size = size - sizeof(heap_block_t);
+    head->free = 1;
+    head->next = 0;
+    head->prev = 0;
+
+    used_memory = 0;
 }
 
-static block_t* find_free_block(uint32_t size)
+static heap_block_t* find_block(uint32_t size)
 {
-    block_t* current = heap_head;
+    heap_block_t* cur = head;
 
-    while(current)
+    while(cur)
     {
-        if(current->free && current->size >= size)
-            return current;
+        if(cur->free && cur->size >= size)
+            return cur;
 
-        current = current->next;
+        cur = cur->next;
     }
 
     return 0;
 }
 
-static void split_block(block_t* block, uint32_t size)
+static void split(heap_block_t* block, uint32_t size)
 {
-    if(block->size <= size + sizeof(block_t))
+    if(block->size <= size + sizeof(heap_block_t))
         return;
 
-    block_t* new_block =
-        (block_t*)((uint8_t*)block +
-        sizeof(block_t) + size);
+    heap_block_t* new_block =
+        (heap_block_t*)((uint8_t*)block + sizeof(heap_block_t) + size);
 
-    new_block->size =
-        block->size - size - sizeof(block_t);
-
+    new_block->size = block->size - size - sizeof(heap_block_t);
     new_block->free = 1;
-    new_block->next = block->next;
 
-    block->size = size;
+    new_block->next = block->next;
+    new_block->prev = block;
+
+    if(block->next)
+        block->next->prev = new_block;
+
     block->next = new_block;
+    block->size = size;
 }
 
 void* kmalloc(uint32_t size)
@@ -72,16 +77,17 @@ void* kmalloc(uint32_t size)
 
     size = (size + 7) & ~7;
 
-    block_t* block = find_free_block(size);
+    heap_block_t* block = find_block(size);
 
     if(!block)
         return 0;
 
-    split_block(block, size);
+    split(block, size);
 
     block->free = 0;
+    used_memory += block->size;
 
-    return (uint8_t*)block + sizeof(block_t);
+    return (uint8_t*)block + sizeof(heap_block_t);
 }
 
 void kfree(void* ptr)
@@ -89,30 +95,24 @@ void kfree(void* ptr)
     if(!ptr)
         return;
 
-    block_t* block =
-        (block_t*)((uint8_t*)ptr -
-        sizeof(block_t));
+    heap_block_t* block =
+        (heap_block_t*)((uint8_t*)ptr - sizeof(heap_block_t));
 
     block->free = 1;
 
-    block_t* current = heap_head;
+    used_memory -= block->size;
 
-    while(current && current->next)
+    // merge
+    if(block->next && block->next->free)
     {
-        if(current->free &&
-           current->next->free)
-        {
-            current->size +=
-                sizeof(block_t) +
-                current->next->size;
+        block->size += sizeof(heap_block_t) + block->next->size;
+        block->next = block->next->next;
+    }
 
-            current->next =
-                current->next->next;
-        }
-        else
-        {
-            current = current->next;
-        }
+    if(block->prev && block->prev->free)
+    {
+        block->prev->size += sizeof(heap_block_t) + block->size;
+        block->prev->next = block->next;
     }
 }
 
@@ -120,12 +120,12 @@ void* kcalloc(uint32_t count, uint32_t size)
 {
     uint32_t total = count * size;
 
-    void* ptr = kmalloc(total);
+    void* p = kmalloc(total);
 
-    if(ptr)
-        memory_set(ptr, 0, total);
+    if(p)
+        memset8((uint8_t*)p, 0, total);
 
-    return ptr;
+    return p;
 }
 
 void* krealloc(void* ptr, uint32_t size)
@@ -139,11 +139,10 @@ void* krealloc(void* ptr, uint32_t size)
         return 0;
     }
 
-    block_t* old_block =
-        (block_t*)((uint8_t*)ptr -
-        sizeof(block_t));
+    heap_block_t* block =
+        (heap_block_t*)((uint8_t*)ptr - sizeof(heap_block_t));
 
-    if(old_block->size >= size)
+    if(block->size >= size)
         return ptr;
 
     void* new_ptr = kmalloc(size);
@@ -151,13 +150,19 @@ void* krealloc(void* ptr, uint32_t size)
     if(!new_ptr)
         return 0;
 
-    memory_copy(
-        new_ptr,
-        ptr,
-        old_block->size
-    );
+    memcpy8(new_ptr, ptr, block->size);
 
     kfree(ptr);
 
     return new_ptr;
+}
+
+uint32_t heap_get_used()
+{
+    return used_memory;
+}
+
+uint32_t heap_get_free()
+{
+    return heap_total_size - used_memory;
 }
